eb4d6e53ecf33c86bfc95cb58dd29dbe4fca8e10
German Rodrigo Alvarez Fabre <geralvarez.085@gmail.com>
2025-08-31 13:49:39 -0300

    feat: add manifest.nix file to tracked files list

---
+++ .config/nix/manifest.nix
@@ -0,0 +1,30 @@
+with import <nixpkgs> {};
+
+[
+  # Git and git tools
+	git
+	lazygit
+
+  # Neovim
+	neovim
+	fd
+	fzf
+	ripgrep
+	xclip
+
+  # Common terminal tools
+	htop
+	zoxide
+	bat
+	eza
+	fastfetch
+	asciinema
+	httpie
+  figlet
+  lolcat
+
+  # favorite terminal emulator and related tools
+  wezterm
+  tmux
+  zellij
+]
